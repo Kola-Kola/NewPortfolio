8d70f06283eb46ce3bddebbebd5c3eb5414f2fa0
Jonathan <jonathan.ibor@gmail.com>
2022-01-13 00:21:57 +0100

    fix: add script to load contact form into <head>

--- components/layouts/PageLayout.tsx
+++ components/layouts/PageLayout.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import Heading from "../blocks/Heading";
 import Menu from "../blocks/Menu";
 import {Menu as ConfigMenu} from "../configs/menu";
-import Script from 'next/script'
 import styled from "styled-components";
 
 const NavContainer = styled.section`
@@ -22,7 +21,6 @@ const PageLayout = (props: any) => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-        <Script src="https://www.cognitoforms.com/f/iframe.js" />
       </Head>
       <NavContainer>
         <div style={{ cursor: 'pointer' }}>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import PageLayout from "../components/layouts/PageLayout";
 import styled from "styled-components";
 import styles from "../styles/Home.module.css";
+import Script from "next/script";
 
 const NavContainer = styled.section`
   display: flex;
@@ -17,6 +18,7 @@ const Contact = () => {
   return (
     <div className={styles.container}>
       <PageLayout page="Contact">
+        <Script src="https://www.cognitoforms.com/f/iframe.js" />
         <iframe src="https://www.cognitoforms.com/f/l_Fw7geWy06ziXnt97Pdvg/1" style={iframeStyle} />
       </PageLayout>
     </div>
